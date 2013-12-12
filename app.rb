4234aa221995e2bf6ad4d342068b277a04ca659b
Chris <chris.haack@me.com>
2013-12-12 17:49:28 -0500

    refactor session logic

--- app.rb
+++ app.rb
@@ -1,20 +1,31 @@
 require 'sinatra/base'
 require './lib/compliment'
-
+require 'debugger'
 # Why is it a good idea to wrap our App class in a module?
 module ComplimentSite
-  class App < Sinatra::Base
+  class App < Sinatra::Application
+    enable :sessions
     get '/' do
-    	@compliment = Compliment.new
+      if session[:last_compliment].nil?
+        @compliment = Compliment.new
+        session[:last_compliment] = @compliment.color
+    	else
+        @compliment = Compliment.new
+        while session[:last_compliment] == @compliment.color 
+          @compliment.color = Compliment::COLORS.sample
+        end
+        session[:last_compliment] = @compliment.color 
+	    end
       erb :compliment
-     		if Compliment.all.size == 1
-     			erb :compliment
-	     	else
-	     		@compliment = Compliment.check_duplicates
-	     		erb :compliment
-	     	end
-	   end
+    end
   end
 end
+
+    # way 3, probably not the best
+    #BLAKE = []
+      # probably not the best
+      # way 2 @@
+
+      #way 1
 
 
--- lib/compliment.rb
+++ lib/compliment.rb
@@ -7,31 +7,39 @@ class Compliment
 
 	COMPLIMENTS = ["You're pretty", "You're super-fantastic!"]
 	IMAGES = ["https://24.media.tumblr.com/cc65a152ab08459fde8e834b30f795a7/tumblr_mxis6kDogx1qdhrnoo1_250.gif", "https://24.media.tumblr.com/b1229bb51705118a2e0be539db74df71/tumblr_mxao3oRohX1sbf1g1o1_500.gif", "http://www.mycathatesyou.com/wp-content/gallery/gifs/6.gif"]
-	COLORS = ["#8C727B", "#8CBEB2", "#F2EBBF", "#F3B562", "#F76363"]
-	HISTORY = []
+	COLORS = ["#8C727B", "#8CBEB2"] #{}"#F2EBBF", "#F3B562", "#F76363"]
+	#HISTORY = []
 
 	def initialize
 		@message = COMPLIMENTS.sample
 		@image = IMAGES.sample
 		@color = COLORS.sample
-		HISTORY << self
-		self.check_duplicates
+		#HISTORY << self
+		#check_duplicates
 	end
 
 	def self.all
 		HISTORY
 	end
 	
-	def self.check_duplicates
-		while (Compliment.all[-2].color == Compliment.all[-1].color) || (Compliment.all[-2].message == Compliment.all[-1].message)
-			self = Compliment.new
-			#Compliment.all[-1].message = COMPLIMENTS.sample
-		end
-	end
+	# def check_duplicates
+	# 	if Compliment.all.size > 1
+	# 		while Compliment.all[-2].color == @color #|| (Compliment.all[-2].
+	# 			@color = COLORS.sample
+	# 		#message == Compliment.all[-1].message)
+	# 		#Compliment.all[-1].message = COMPLIMENTS.sample
+	# 		end
+	# 	end
+	# end
 
 end
 
 # comp = Compliment.new
-# p Compliment.all.size
-# p Compliment.check_duplicates
+# comp1 = Compliment.new
+# comp2 = Compliment.new
+# comp3 = Compliment.new
+# comp4 = Compliment.new
+# comp5 = Compliment.new
+# comp6 = Compliment.new
+# Compliment.all.each {|compliment| puts compliment.color}
 
