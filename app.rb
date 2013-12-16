093d7cf456cf45324cd581c8006f0daafab49fba
Chris <chris.haack@me.com>
2013-12-15 20:10:05 -0500

    clean up comments, remove debugger

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require 'sinatra/base'
 require './lib/compliment'
-require 'debugger'
-# Why is it a good idea to wrap our App class in a module?
+
 module ComplimentSite
   class App < Sinatra::Application
     enable :sessions
